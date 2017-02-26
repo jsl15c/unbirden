3582a3c6126e24466e540436e45cc7a03d64bd7e
Jarrod Luca <jarrodluca@gmail.com>
2017-02-25 23:39:58 -0500

    add new files for flask - app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import flask
+from flask import Flask, render_template, request
+from twitter import getTweets
 
+import json
+from watson_developer_cloud import AlchemyLanguageV1
+alchemy_language = AlchemyLanguageV1(api_key='a04bf0cda38fd380a2e89b9b54d6076729b568ce')
 
 app = Flask(__name__)
+
 
 # routing/mapping to tie url to python
 # function
 @app.route('/')
-def index();
-    return 'This is the homepage'
+def index():
+    return render_template('input.html')
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      handle = request.form['handle']
+      tweets = getTweets(handle)
+      print tweets
+      return (list(tweets))
